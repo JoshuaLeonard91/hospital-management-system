04b7f62375675c1fe9a5e5f6e02c6e91a3160a94
JoshuaLeonard91 <127473978+JoshuaLeonard91@users.noreply.github.com>
2024-12-17 17:29:47 -0500

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", // Adjust based on your project structure
